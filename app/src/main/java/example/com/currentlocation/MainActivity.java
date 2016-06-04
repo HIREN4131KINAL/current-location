9db9d0edf4105aa303a4260afea70fd18163fc0a
@rpit <arpitpatel009@gmail.com>
2016-06-04 16:23:43 +0530

    Get address from Lag and Lat

--- app/src/main/java/example/com/currentlocation/MainActivity.java
+++ app/src/main/java/example/com/currentlocation/MainActivity.java
@@ -4,18 +4,32 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends Activity implements LocationListener {
 
     private LocationManager locationManager;
+    double Logitude, Latitude;
+    TextView locationLabel, tv_address;
+    Geocoder geocoder;
+    List<Address> addresses;
+    String city, state, country, postalCode, knownName, address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +38,53 @@ public class MainActivity extends Activity implements LocationListener {
         setContentView(R.layout.activity_main);
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationLabel = (TextView) findViewById(R.id.locationLabel);
+        tv_address = (TextView) findViewById(R.id.tv_address);
 
-
-       /* locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+          /* locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                 2000, 1, this);*/
 
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
                 0, 0, this);
 
+    }
 
+    // For getting address and all details
+    private String getCompleteAddressString(double LATITUDE, double LONGITUDE) {
+        String strAdd = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(LATITUDE, LONGITUDE, 1);
+            if (addresses != null) {
+                Address returnedAddress = addresses.get(0);
+                StringBuilder strReturnedAddress = new StringBuilder("");
+
+                for (int i = 0; i < returnedAddress.getMaxAddressLineIndex(); i++) {
+                    strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+                }
+                strAdd = strReturnedAddress.toString();
+                Log.w("My Current loction address", "" + strReturnedAddress.toString());
+            } else {
+                Log.w("My Current loction address", "No Address returned!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.w("My Current loction address", "Canont get Address!");
+        }
+        return strAdd;
     }
 
     @Override
     public void onLocationChanged(Location location) {
 
+        Logitude = location.getLongitude();
+        Latitude = location.getLatitude();
+
         String msg = "New Latitude: " + location.getLatitude()
                 + "New Longitude: " + location.getLongitude();
 
         Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+
     }
 
     @Override
@@ -67,11 +110,20 @@ public class MainActivity extends Activity implements LocationListener {
     }
 
     public void GetLocation(View view) {
+
+        locationLabel.setText("Logitude = " + Logitude + " \n Latitude = " + Latitude);
+
     }
 
     public void Disconnect(View view) {
+
+        address = getCompleteAddressString(Latitude, Logitude);
+        tv_address.setText(address);
+
     }
 
     public void Connect(View view) {
+
     }
+
 }
